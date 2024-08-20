bb34e5f69b650ebec8a0529ad4be77e93fbf5e6f
bilalbhati2121 <bilalbhati011@gamil.com>
2024-08-20 20:23:03 +0530

    url5

--- config/db.js
+++ config/db.js
@@ -20,10 +20,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/', {
-      useNewUrlParser: true,
-      // Removed `useUnifiedTopology` as it's no longer necessary
-    });
+    await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
