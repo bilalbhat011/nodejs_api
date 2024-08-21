b6807858f523449672ad47abd5c362697b2b2d15
bilalbhati2121 <bilalbhati011@gamil.com>
2024-08-21 11:40:12 +0530

    database

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+   
     });
     console.log('MongoDB connected');
   } catch (error) {
