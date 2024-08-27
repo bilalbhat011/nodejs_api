91708bd33c30fb787c9fbe8a78439e8666f6fa86
bilalbhati2121 <bilalbhati011@gamil.com>
2024-08-27 17:41:25 +0530

    file update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,49 @@
+// import mongoose from 'mongoose';
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/',{
+   
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection failed:', error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+
+
+
+// import mongoose from 'mongoose';
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/demoDatabase', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection failed:', error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+
+
+
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/', {
-   
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
     console.log('MongoDB connected');
   } catch (error) {
--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-import dotenv from "dotenv"
-dotenv.config()
+// import dotenv from "dotenv"
+// dotenv.config()
+
+// import express from 'express';
+// import connectDB from './config/db.js';
+// import { user } from './rotues/user.routes.js';
+// import cors from "cors";
+// // import authRoutes from './routes/auth.js';
+
+// const app = express();
+// app.use(express.json());  
+// connectDB();
+// app.use(cors());
+
+// app.use(user)
+
+// // app.use(express.json());
+// // app.use('/api/auth', authRoutes);
+
+// app.listen(process.env.PORT || 5000,(request,response) =>{
+//   console.log("Yes your server connected with PORT:3002");
+// })
+
+
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from 'express';
 import connectDB from './config/db.js';
-import { user } from './rotues/user.routes.js';
 import cors from "cors";
-// import authRoutes from './routes/auth.js';
+import { user } from "./rotues/user.routes.js";
 
 const app = express();
 app.use(express.json());  
 connectDB();
 app.use(cors());
 
-app.use(user)
+app.use(user);
 
-// app.use(express.json());
-// app.use('/api/auth', authRoutes);
-
-app.listen(process.env.PORT || 5000,(request,response) =>{
-  console.log("Yes your server connected with PORT:3002");
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Yes, your server is connected with PORT: ${process.env.PORT || 5000}`);
+});
