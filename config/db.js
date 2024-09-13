e83f31ebcd2ad60b0d8308d866b68eb7376b1d95
bilalbhati2121 <bilalbhati011@gamil.com>
2024-09-13 17:43:39 +0530

    upload images

--- config/db.js
+++ config/db.js
@@ -17,31 +17,11 @@
 
 
 
-// import mongoose from 'mongoose';
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/demoDatabase', {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     });
-//     console.log('MongoDB connected');
-//   } catch (error) {
-//     console.error('MongoDB connection failed:', error.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-
-
-
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect('mongodb+srv://mohammadbhati2121:wIPLiEUyGoZ6tOWi@demodatabase.im9wljz.mongodb.net/demoDatabase', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
@@ -53,3 +33,8 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
+
+
+
